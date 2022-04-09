7aa8913beb4cb383375960c021491b6d93925b06
Hobyy19 <arivonyhoby@gmail.com>
2022-04-09 21:41:24 +0300

    update package.json backend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 mongoose
-  .connect('mongodb+srv://e-kaly:e-kaly@cluster0.f3m1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  // .connect('mongodb+srv://e-kaly:e-kaly@cluster0.f3m1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .connect('mongodb://127.0.0.1:27017/e-kaly')
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -13,11 +14,11 @@ mongoose
     console.error('Error connecting to mongo', err.reason)
 })
 
-const userRoute = require('../backend/routes/user.route')
-const platRoute = require('../backend/routes/plat.route')
-const commandeRoute = require('../backend/routes/commande.route')
-const livraisonRoute = require('../backend/routes/livraison.route')
-const commande_platRoute = require('../backend/routes/livraison.route')
+const userRoute = require('./routes/user.route')
+const platRoute = require('./routes/plat.route')
+const commandeRoute = require('./routes/commande.route')
+const livraisonRoute = require('./routes/livraison.route')
+const commande_platRoute = require('./routes/livraison.route')
 
 
 const app = express()
@@ -52,3 +53,88 @@ app.use(function (err, req, res, next) {
     if (!err.statusCode) err.statusCode = 500 
     res.status(err.statusCode).send(err.message) 
 })
+
+// ANOTHER EXAMPLE
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const MongoClient = require('mongodb').MongoClient
+// const app = express()
+
+
+
+// // const connectionString = process.env.DB_URL
+// const connectionString = 'mongodb://127.0.0.1:27017/e-kaly'
+
+// MongoClient.connect(connectionString, { useUnifiedTopology: true })
+//   .then(client => {
+//     console.log('Connected to Database')
+//     const db = client.db('e-kaly')
+//     const userCollection = db.collection('user')
+
+//     // ========================
+//     // Middlewares
+//     // ========================
+//     app.use(bodyParser.urlencoded({ extended: true }))
+//     app.use(bodyParser.json())
+
+//     // ========================
+//     // Routes
+//     // ========================
+//     app.get('/api/user/', (req, res) => {
+//       db.collection('user').find().toArray()
+//         .then(users => {
+//           console.log(users)
+//           res.json(users)
+//         })
+//         .catch(/* ... */)
+//     })
+
+//     // app.post('/quotes', (req, res) => {
+//     //   quotesCollection.insertOne(req.body)
+//     //     .then(result => {
+//     //       res.redirect('/')
+//     //     })
+//     //     .catch(error => console.error(error))
+//     // })
+
+//     // app.put('/quotes', (req, res) => {
+//     //   quotesCollection.findOneAndUpdate(
+//     //     { name: 'Yoda' },
+//     //     {
+//     //       $set: {
+//     //         name: req.body.name,
+//     //         quote: req.body.quote
+//     //       }
+//     //     },
+//     //     {
+//     //       upsert: true
+//     //     }
+//     //   )
+//     //     .then(result => res.json('Success'))
+//     //     .catch(error => console.error(error))
+//     // })
+
+//     // app.delete('/quotes', (req, res) => {
+//     //   quotesCollection.deleteOne(
+//     //     { name: req.body.name }
+//     //   )
+//     //     .then(result => {
+//     //       if (result.deletedCount === 0) {
+//     //         return res.json('No quote to delete')
+//     //       }
+//     //       res.json('Deleted Darth Vadar\'s quote')
+//     //     })
+//     //     .catch(error => console.error(error))
+//     // })
+
+//     // ========================
+//     // Listen
+//     // ========================
+//     const isProduction = process.env.NODE_ENV === 'production'
+//     const port = isProduction ? 7500 : 3000
+//     app.listen(port, function () {
+//       console.log(`listening on ${port}`)
+//     })
+//   })
+//   .catch(console.error)
