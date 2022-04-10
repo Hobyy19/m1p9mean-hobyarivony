1e16a496458e2c7795b9d76cd121f92c784646c8
Hobyy19 <arivonyhoby@gmail.com>
2022-04-10 18:21:09 +0300

    update package.json

--- frontend/server.js
+++ frontend/server.js
@@ -1,33 +1,33 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/m1p9mean-hobyarivony'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/m1p9mean-hobyarivony'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/m1p9mean-hobyarivony/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8090);
-
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
-
-var port = process.env.PORT || 8000;
-
-var path = require('path');
-var rootPath = path.normalize(__dirname + '/');
-app.use(express.static(rootPath + '/app'));
-
-
-
-app.listen(port, function (err) {
-  console.log('running server on port ' + port);
+res.sendFile(path.join(__dirname+'/dist/m1p9mean-hobyarivony/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8000);
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+
+// var app = express();
+
+// var port = process.env.PORT || 8000;
+
+// var path = require('path');
+// var rootPath = path.normalize(__dirname + '/');
+// app.use(express.static(rootPath + '/app'));
+
+
+
+// app.listen(port, function (err) {
+//   console.log('running server on port ' + port);
+// });
