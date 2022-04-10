d464a4aa0c0b9040271accc685ab97396c336ed0
Hobyy19 <arivonyhoby@gmail.com>
2022-04-10 14:57:07 +0300

    Server.js frontend

--- frontend/server.js
+++ frontend/server.js
@@ -1,61 +1,21 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
-var database;
-
-var USER_COLLECTION = "user";
-
-var app = express();
-app.use(bodyParser.json());
-
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-
-// Local database URI.
-const LOCAL_DATABASE = "mongodb://localhost:27017/e-kaly";
-// Local port.
-const LOCAL_PORT = 8080;
-
-// Init the server
-mongodb.MongoClient.connect(process.env.MONGODB_URI || LOCAL_DATABASE,
-{
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-}, function (error, client) {
-    // Check if there are any problems with the connection to MongoDB database.
-    if (error) {
-        console.log(error);
-        process.exit(1);
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
     }
-    // Save database object from the callback for reuse.
-    database = client.db();
-    console.log("Database connection done.");
-    // Initialize the app.
-    var server = app.listen(process.env.PORT || LOCAL_PORT, function () {
-        var port = server.address().port;
-        console.log("App now running on port", port);
-    });
+    next();
+}
+
+const express = require('express');
+const app = express();
+
+
+app.use(requireHTTPS);
+app.use(express.static(`./dist/m1p9mean-hobyarivony`));
+
+app.get(`/*`, function(req, res) {
+    res.sendFile('index.html', {root: 'dist/m1p9mean-hobyarivony/'}
+  );
 });
 
-/*  "/api/user/add"
- *   POST: creates a new user
- */
-app.post("/api/user/add", function (req, res) {
-    var user = req.body;
-
-    if (!user.email) {
-        manageError(res, "Invalid input", "Email is mandatory.", 400);
-    } else if (!user.password) {
-        manageError(res, "Invalid input", "Password is mandatory.", 400);
-    } else {
-        database.collection(USER_COLLECTION).insertOne(user, function (err, doc) {
-            if (err) {
-                manageError(res, err.message, "Failed to create new product.");
-            } else {
-                res.status(201).json(doc.ops[0]);
-            }
-        });
-    }
-});
+app.listen(process.env.PORT || 8090);
